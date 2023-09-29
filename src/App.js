1f866a68053008265cbde772980605b5f895d3b3
Porfirio-Prodigy <prodigy.porfirio@gmail.com>
2023-09-29 13:35:16 -0300

    feat: landing page Lapa Atlas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,241 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-full">
+        <div className='header-container'>
+          <header className='header-content'>
+              <a href='#'>
+                <img src='https://www.lapa.ninja/lab/atlas/img/logo.png' className='logo' alt='logo'/>
+              </a>
+              <a href="#" className="get-access">
+                Get Early Access
+              </a>
+          </header>
+          <h1 className='main-title'>
+            A New Way
+            <br/>
+            To Start Business
+          </h1>
+
+          <p className='header-text'>
+            Lorem ipsum dolor sit amet, id nec enim autem oblique, ei dico mentitum duo.
+            <br/>
+            Illum iusto laoreet his te. Lorem partiendo mel ex. Ad vitae admodum voluptatum per.
+          </p>
+          <div className='get-start-container'>
+            <a href='#' className='get-start'>
+              Get Started
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <main>
+        <div className='main-content'>
+          
+          <div className='benefit-bar'>
+            <h2 className='subtitle'>
+              Smartest protection for your site
+            </h2>
+            <div className='benefit-bar-benefits'>
+              <div className='benefit'>
+                  <img src="https://www.lapa.ninja/lab/atlas/img/smart-protect-1.jpg" alt="smart-project" className='benefit-image'/>
+                  <h3 className='benefit-title'>Anti-spam</h3>
+                  <p className='benefit-text'>Lorem ipsum dolor sit amet porro his no his deleniti</p>
+              </div>
+
+              <div className='benefit'>
+                  <img src="https://www.lapa.ninja/lab/atlas/img/smart-protect-2.jpg" alt="smart-project" className='benefit-image'/>
+                  <h3 className='benefit-title'>Phishing Detect</h3>
+                  <p className='benefit-text'>Ne error antiopam usu. Sed vocen concludaturque ea</p>
+              </div>
+
+              <div className='benefit'>
+                  <img src="https://www.lapa.ninja/lab/atlas/img/smart-protect-3.jpg" alt="smart-project" className='benefit-image'/>
+                  <h3 className='benefit-title'>Smart Scan</h3>
+                  <p className='benefit-text'>Et usu ocurreret elaboraret doctus prodesse assueverit.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='features'>
+            <div className='feature'>
+              <div className='feature-full'>
+                <div className='feature-content'>
+                  <h2 className='feature-title'>Take a look inside</h2>
+                  <p className='feature-text'>
+                    Te iisque labitur eos, nec sale argumentum scribentur no,
+                    <br/>
+                    augue disputando in vim. Erat fugit sit at, ius lorem deserunt deterruisset no.
+                  </p>
+                  <a href='#' className='feature-link'>Learn More</a>
+                </div>
+              
+              
+                <img src='https://www.lapa.ninja/lab/atlas/img/feature-1.png' alt='feature-image' className='feature-image'/>
+              </div>
+            </div>
+
+            <div className='feature reverse'>
+              <div className='feature-full'>
+                <div className='feature-content'>
+                  <h2 className='feature-title'>Safe and reliable</h2>
+                  <p className='feature-text'>
+                    Duo suas detracto maiestatis ad, commodo lucilius invenire nec ad,
+                    <br/>
+                    eum et oratio disputationi. Falli lobortis his ad
+                  </p>
+                  <a href='#' className='feature-link'>Learn More</a>
+                </div>
+                <img src='https://www.lapa.ninja/lab/atlas/img/feature-2.png' alt='feature-image' className='feature-image'/>
+              </div>
+            </div>
+          </div>
+
+          <div className='pricing'>
+            <div class="pricing-container">
+              <h2 class="pricing-title">Check our pricing</h2>
+
+              <div className='princing-porpuses'>
+                <div className='pricing-porpuse'>
+                  <h3 className='pricing-title'>STARTUP</h3>
+
+                  <p class="princing-price">
+                    $ 
+                    <span class="pricing-price-bold">0</span> 
+                    / MO.
+                  </p>
+
+                  <ul className='pricing-benefits'>
+                    <li className='pricing-benefit'>Up to 5 Documents</li>
+                    <li className='pricing-benefit'>Up to 3 Reviews</li>
+                    <li className='pricing-benefit'>5 team Members</li>
+                    <li className='pricing-benefit'>Limited Support</li>
+                  </ul>
+
+                  <a href='#' className='pricing-link'>Get Free</a>
+                </div>
+
+                <div className='pricing-porpuse invert'>
+                  <h3 className='pricing-title'>PREMIUM</h3>
+
+                  <p class="princing-price">
+                    $ 
+                    <span class="pricing-price-bold">10</span> 
+                    / MO.
+                  </p>
+
+                  <ul className='pricing-benefits'>
+                    <li className='pricing-benefit'>Up to 15 Documents</li>
+                    <li className='pricing-benefit'>Up to 10 Reviews</li>
+                    <li className='pricing-benefit'>25 team Members</li>
+                    <li className='pricing-benefit'>Limited Support</li>
+                  </ul>
+
+                  <a href='#' className='pricing-link'>Get Free</a>
+                </div>
+
+                <div className='pricing-porpuse'>
+                  <h3 className='pricing-title'>PROFESSIONAL</h3>
+
+                  <p class="princing-price">
+                    $ 
+                    <span class="pricing-price-bold">30</span> 
+                    / MO.
+                  </p>
+
+                  <ul className='pricing-benefits'>
+                    <li className='pricing-benefit'>Unlimited Documents</li>
+                    <li className='pricing-benefit'>Unlimited Reviews</li>
+                    <li className='pricing-benefit'>Unlimited Members</li>
+                    <li className='pricing-benefit'>Unlimited Support</li>
+                  </ul>
+
+                  <a href='#' className='pricing-link'>Get Free</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='partner-brand'>
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-1.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-2.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-3.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-4.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-5.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+
+            <div className='partner-brand-container'>
+              <img src='https://www.lapa.ninja/lab/atlas/img/client-6.png' alt="partner brand" className='partner-brand-image' />
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className='footer-full'>
+        <div className='footer-container'>
+          <div className='footer-form'> 
+            <div className='footer-form-content'>
+              <h2 className='footer-form-title'>Contact Us</h2>
+
+              <p className='footer-form-text'>
+                Te iisque labitur eos, nec sale argumentum scribentur,
+                <br/>
+                augue disputando in vim. Erat fugit sit at, ius lorem.
+              </p>
+
+              <ul className='footer-form-contact'>
+                <li className='footer-form-info'>Email : company_email@com</li>
+                <li className='footer-form-info'>Phone : 361-688-5824</li>
+                <li className='footer-form-info'>Address : 4826 White Avenue, Corpus Christi, Texas</li>
+              </ul>
+            </div>
+
+            <form className='contact-form'>
+              <div className='contact-form-fields'>
+                <div className='contact-form-field'>
+                  <label for="name">Your Name</label>
+                  <input type="name" class="form-control" id="name" />
+                </div>
+
+                <div className='contact-form-field'>
+                  <label for="email">Your email</label>
+                  <input type="email" class="form-control" id="email" />
+                </div>
+              </div>
+              <div className='contact-form-field'>
+                  <label for="email">Message</label>
+                  <textarea className='form-control'>
+
+                  </textarea>
+              </div>
+
+              <button type='submit' className='submit-button'>Submit</button>
+            </form>
+          </div>
+
+          <div className='footer-developed-by'>
+            <p className='developed-by'>
+              Developed by João Porfirio, 2023. Based on: <a href='https://www.lapa.ninja/lab/atlas/' alt="based on">Lapa.ninja</a>
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
